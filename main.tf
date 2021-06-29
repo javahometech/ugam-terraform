5d683a5e115d048e87d19bf1149d2982a1b076bc
Hari Kammana <hari.kammana@gmail.com>
2021-06-29 10:30:45 +0530

    module outputs

--- main.tf
+++ main.tf
@@ -9,3 +9,14 @@ module "myapp_vpc" {
   }
   app_name = "ugam-api"
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0ab4d1e9cf9a1215a"
+  instance_type = "t2.micro"
+  subnet_id   = module.myapp_vpc.pub_sub_ids[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
